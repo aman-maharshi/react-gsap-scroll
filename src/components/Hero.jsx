79c3672a482f0cb17176bc3c6e090372d60e14f3
aman-maharshi <amanm4harshi@gmail.com>
2025-10-17 21:46:17 +0530

    hero lg fixes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,10 @@ const Hero = () => {
   return (
     <section id="hero">
       <div>
-        <h1>Mac Book Pro</h1>
+        <h1 className="relative">
+          Mac Book Pro
+          <span className="absolute -top-2 -right-6 text-[10px] lg:text-xs opacity-70">2025</span>
+        </h1>
         <img src="/title.png" alt="Mac Book Title" />
         <video ref={videoRef} src="/videos/hero.mp4" autoPlay muted playsInline />
         <button onClick={handleBuyClick}>Buy</button>
