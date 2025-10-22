f12ee84e8ec2c48b0146e7e0d3d68736773d2303
aman-maharshi <amanm4harshi@gmail.com>
2025-10-22 20:50:35 +0530

    showcase section updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import NavBar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Product from "./components/Product"
+import Showcase from "./components/Showcase"
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <NavBar />
       <Hero />
       <Product />
+      <Showcase />
     </main>
   )
 }
