fce38c7ac991a8324583ca37fe6672e14372a66e
aman-maharshi <amanm4harshi@gmail.com>
2025-10-22 21:02:32 +0530

    section updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import NavBar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Product from "./components/Product"
 import Showcase from "./components/Showcase"
+import Performance from "./components/Performance"
+import Features from "./components/Featuers"
+import Highlights from "./components/Highlights"
+import Footer from "./components/Footer"
 
 const App = () => {
   return (
@@ -10,7 +14,11 @@ const App = () => {
       <NavBar />
       <Hero />
       <Product />
-      <Showcase />
+      {/* <Showcase />
+      <Performance />
+      <Features /> */}
+      <Highlights />
+      <Footer />
     </main>
   )
 }
